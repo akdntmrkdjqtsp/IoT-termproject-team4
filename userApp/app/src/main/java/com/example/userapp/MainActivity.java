b208d5f10c8e586b43aaf6e1e2df410e8de34045
joeun-01 <jjy020121@gmail.com>
2023-06-15 10:32:05 +0900

    목적지 수정

--- userApp/app/src/main/java/com/example/userapp/MainActivity.java
+++ userApp/app/src/main/java/com/example/userapp/MainActivity.java
@@ -40,45 +40,83 @@ public class MainActivity extends AppCompatActivity {
     private String getClassroom() {
         String destination = spinner.getSelectedItem().toString();
 
+        /*
+            <item>406호</item>
+            <item>407호</item>
+            <item>413호</item>
+            <item>414호</item>
+            <item>4층 아르테크네</item>
+            <item>4층 휴게 공간</item>
+            <item>4층 엘리베이터(동)</item>
+            <item>4층 엘리베이터(중)</item>
+            <item>4층 계단</item>
+            <item>506호</item>
+            <item>508호</item>
+            <item>510호</item>
+            <item>513호</item>
+            <item>큐브 입구 1</item>
+            <item>큐브 입구 2</item>
+            <item>5층 엘리베이터 1(동)</item>
+            <item>5층 엘리베이터 2(중)</item>
+            <item>5층 엘리베이터 3(서)</item>
+            서쪽이 3 중앙이 4 동이 1
+         */
+
         switch (destination) {
-            case "405호" :
-                destination = "405";
+            case "406호" :
+                destination = "406";
                 break;
-
-            case "412호" :
-                destination = "412";
+            case "407호" :
+                destination = "407-indoor";
                 break;
-
-            case "418호" :
-                destination = "418";
+            case "413호" :
+                destination = "413-indoor";
                 break;
-
-            case "433호" :
-                destination = "433";
+            case "414호" :
+                destination = "414-indoor";
                 break;
-
             case "4층 아르테크네" :
                 destination = "artechne-4";
                 break;
-
-            case "505호" :
-                destination = "505";
+            case "4층 휴게 공간" :
+                destination = "rest-area";
+                break;
+            case "4층 엘리베이터(동)" :
+                destination = "elevator-1-4F";
+                break;
+            case "4층 엘리베이터(중)" :
+                destination = "elevator-4-4F";
+                break;
+            case "4층 계단" :
+                destination = "stair-5-4F";
                 break;
 
-            case "512호" :
-                destination = "512";
+            case "506호" :
+                destination = "506";
                 break;
-
-            case "520호" :
-                destination = "520";
+            case "508호" :
+                destination = "508-indoor";
                 break;
-
-            case "531호" :
-                destination = "531";
+            case "510호" :
+                destination = "510";
                 break;
-
-            case "5층 아르테크네" :
-                destination = "artechne-5";
+            case "513호" :
+                destination = "513";
+                break;
+            case "큐브 입구 1" :
+                destination = "cube";
+                break;
+            case "큐브 입구 2" :
+                destination = "cube";
+                break;
+            case "5층 엘리베이터(동)" :
+                destination = "elevator-1-5F";
+                break;
+            case "5층 엘리베이터(중)" :
+                destination = "elevator-4-5F";
+                break;
+            case "5층 엘리베이터(서)" :
+                destination = "elevator-3-5F";
                 break;
         }
 
